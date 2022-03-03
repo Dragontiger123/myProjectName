f5d8115bf412ce325cb6346891d041e419c2bdb3
admin <admin@DESKTOP-S0CR8K9>
2022-03-03 14:29:12 +0900

    commit

---
+++ src/com/human/ex/JavaTest01.java
@@ -0,0 +1,30 @@
+package com.human.ex;
+
+public class JavaTest01 {
+
+	public static void main(String[] args) {
+		/*
+		 * 자동으로 아래는 생성된다.
+		 */
+		System.out.println("HelloWorld");									
+		System.out.println("HelloWorld");	
+		System.out.println("HelloWorld");
+		int a =5;	
+		int b =6;
+		if (a>b) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+			// shift + ctr +방향키
+            // ctr + 방향키
+			// shift + 방향키
+            // ctr + / 주석 설정 및 해제
+			// home, end
+			// ctr +, - 글자,폰트 조절+
+			// ctr + f 모두 바꾸기
+			// ctr + z 취소
+			// ctr + y 복구
+			// ctr + c,v,x 복사, 붙여넣기, 잘라내기
+		}		
+	}
+}
---
+++ src/com/human/ex/JavaTest02.java
@@ -0,0 +1,73 @@
+package com.human.ex;
+
+public class JavaTest02 {
+	
+	public static void main(String[] args) {
+		// . 괄호 = , 에서 줄을 변경하여 사용할 수 있다
+		System.out.println("문제1");
+		System.out.println(
+				"Hello World~"
+				);
+		System.out.println("\n");
+		//큰따옴표(" ")는 문자열(String)을 감싸는 기호이고
+	    //작은따옴표(' ')는 문자(Character)를 감싸는 기호이다
+		
+		/*
+		 * 문제2
+		 * 키워드 : public , class, void
+		 * 예약어 : public , class, void
+		 * 식별자 : com.human.ex , JavaTest02
+		 */
+
+		System.out.println("문제3");
+		System.out.println("안녕자바");
+		System.out.println("\n");
+		
+		System.out.println("문제4");
+		System.out.println("*******************************");
+		System.out.println("* 이름 : 김용범                   *");
+		System.out.println("* 이메일 : bumil1203@naver.com   *");
+		System.out.println("* 전화번호 : 010-7774-1607       *");
+		System.out.println("*******************************");
+		System.out.println("\n");
+		
+		System.out.println("문제5");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		System.out.println("    *");
+		System.out.println("   **");
+		System.out.println("  ***");
+		System.out.println(" ****");
+		System.out.println("*****");
+		System.out.println("\n");
+		
+		System.out.println("문제6");
+		System.out.println("*****************");
+		System.out.println("*국어\t50\t*");
+		System.out.println("*****************");
+		System.out.println("*영어\t50\t*");
+		System.out.println("*****************");
+		System.out.println("*수학\t50\t*");
+		System.out.println("*****************");
+		System.out.println("*컴퓨터 학과   홍길동*");
+		System.out.println("*****************");
+		System.out.println("\n");
+		
+		System.out.println("문제7");
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		System.out.println("1\t2\t3\t4\t5\t6\t7");
+		System.out.println("8\t9\t10\t11\t12\t13\t14");
+		System.out.println("15\t16\t17\t18\t19\t20\t21");
+		System.out.println("22\t23\t24\t25\t26\t27\t28");
+		System.out.println("29\t30\t31");
+	}
+
+}
